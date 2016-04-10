11e97b7c5ea200b91016a15d3cbd7721e65bbc03
alexeimotkoff <alexeimotkoff@gmail.com>
2016-04-10 11:47:38 +0600

    Реализовано хранилище данны�

--- online_book_store.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ online_book_store.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -6,6 +6,7 @@ using Moq;
 using Ninject;
 using online_book_store.Domain.Abstract;
 using online_book_store.Domain.Entities;
+using online_book_store.Domain.Concrete;
 
 namespace online_book_store.WebUI.Infrastructure
 {
@@ -31,14 +32,7 @@ namespace online_book_store.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IBookRepository> mock = new Mock<IBookRepository>();
-            mock.Setup(m => m.Books).Returns(new List<Book>
-    {
-        new Book { Name = "fg", Price = 1488 },
-        new Book { Name = "ff", Price=2299 },
-        new Book { Name = "rr", Price=899.4M }
-    });
-            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+            kernel.Bind<IBookRepository>().To<EFBookRepository>();
         }
     }
 }
