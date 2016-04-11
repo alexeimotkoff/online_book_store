4dc2c886ad03f41c785c0dc5c6db990134bcd00e
Ihatemyfantasy <dafna.fokina@gmail.com>
2016-04-12 00:27:14 +0600

    Разбиение на страницы (2)

--- online_book_store.WebUI/App_Start/RouteConfig.cs
+++ online_book_store.WebUI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace online_book_store.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Book", action = "List" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Book", action = "List", id = UrlParameter.Optional }
