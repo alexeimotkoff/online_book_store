293d7b1bedd26d269937b07f5f55d1c10384dca6
Ihatemyfantasy <dafna.fokina@gmail.com>
2016-04-11 06:55:19 +0600

    Разбиение на страницы

--- online_book_store.WebUI/Controllers/BookController.cs
+++ online_book_store.WebUI/Controllers/BookController.cs
@@ -5,19 +5,34 @@ using System.Web;
 using System.Web.Mvc;
 using online_book_store.Domain.Abstract;
 using online_book_store.Domain.Entities;
+using online_book_store.WebUI.Models;
 
 namespace online_book_store.WebUI.Controllers
 {
     public class BookController : Controller
     {
         private IBookRepository repository;
+        public int pageSize = 4;
        public BookController(IBookRepository repo)
         {
             repository = repo;
         }
-       public ViewResult List()
+       public ViewResult List(int page = 1)
        {
-           return View(repository.Books);
+           BooksListViewModel model = new BooksListViewModel
+           {
+               Books = repository.Books
+               .OrderBy(book => book.BookId)
+               .Skip((page - 1) * pageSize)
+               .Take(pageSize),
+               PagingInfo = new PagingInfo
+               {
+                   CurrentPage = page,
+                   ItemsPerPage = pageSize,
+                   TotalItems = repository.Books.Count()
+               }
+           };
+           return View(model);
        }
 	}
 }
---
+++ online_book_store.WebUI/Models/BooksListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using online_book_store.Domain.Entities;
+
+namespace online_book_store.WebUI.Models
+{
+    public class BooksListViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
