c25e07d326dc3a411f385568f6a37119ff45eda9
Ihatemyfantasy <dafna.fokina@gmail.com>
2016-05-05 22:38:26 +0600

    Админпанель(начало)

---
+++ online_book_store.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using online_book_store.Domain.Abstract;
+
+namespace online_book_store.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IBookRepository repository;
+
+        public AdminController(IBookRepository repo)
+        {
+            repository = repo;
+        }
+        public ActionResult Index()
+        {
+            return View(repository.Books);
+        }
+	}
+}
