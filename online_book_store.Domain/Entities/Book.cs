3d7d44819b3783f04d0a033e6efde7a41a6327d8
alexeimotkoff <alexeimotkoff@gmail.com>
2016-05-13 22:34:38 +0600

    начало

--- online_book_store.Domain/Entities/Book.cs
+++ online_book_store.Domain/Entities/Book.cs
@@ -34,5 +34,7 @@ namespace online_book_store.Domain.Entities
         [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите положительное значение для цены")]
         public decimal Price { get; set; }
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
     }
 }
